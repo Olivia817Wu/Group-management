0996d89b31607992f54673a5abc2df26914fb838
Olivia817Wu <Yunqi.Yang0104@outlook.com>
2025-04-26 11:48:44 +0100

    Update settings.py

--- Movie_recommendation_system/settings.py
+++ Movie_recommendation_system/settings.py
@@ -61,13 +61,9 @@ WSGI_APPLICATION = 'Movie_recommendation_system.wsgi.application'
 # 数据库配置
 
 DATABASES = {
-      'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'db_movie_recommend',
-        'USER': 'DB_USER,
-        'PASSWORD': 'DB_PASSWORD‘,
-        'HOST': 'DB_HOST‘,
-        'PORT': 'DB_PORT‘
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
