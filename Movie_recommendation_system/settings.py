1031aef2337b44deea476bed293a7c389dc82338
Olivia817Wu <Yunqi.Yang0104@outlook.com>
2025-04-26 12:24:21 +0100

    Update settings.py

--- Movie_recommendation_system/settings.py
+++ Movie_recommendation_system/settings.py
@@ -60,11 +60,12 @@ WSGI_APPLICATION = 'Movie_recommendation_system.wsgi.application'
 
 # 数据库配置
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
 }
 
 
