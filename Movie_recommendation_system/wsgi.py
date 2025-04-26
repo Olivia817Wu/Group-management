ed166e68bc129e7cea3b3858b4feeafba4902628
Olivia817Wu <Yunqi.Yang0104@outlook.com>
2025-04-26 13:05:24 +0100

    Update wsgi.py

--- Movie_recommendation_system/wsgi.py
+++ Movie_recommendation_system/wsgi.py
@@ -1,11 +1,7 @@
 import os
+
 from django.core.wsgi import get_wsgi_application
-import django
-from django.core.management import call_command
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Movie_recommendation_system.settings')
-
-django.setup()
-call_command('migrate')  # 自动执行migrate
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Movie_recommendation_system.settings")
 
 application = get_wsgi_application()
